93e7fb37cd896b852eee99bac255e76a3193b85d
Alfalmi <pinooxd@gmail.com>
2021-09-30 18:59:10 -0700

    commit

---
+++ src/lib/commerce.js
@@ -0,0 +1,6 @@
+import Commerce from "@chec/commerce.js";
+// pk_339413a736208e28ce5f3998d765ebeaa21c11b481f50
+export const commerce = new Commerce(
+    'pk_339413a736208e28ce5f3998d765ebeaa21c11b481f50',
+  true
+);
